b0ff04269a219bf7edb4ed9fba25cf82206816b0
snowapril <sinjihng@gmail.com>
2022-02-21 15:02:28 +0900

    Update vulkan framework precompiled header

--- VulkanFramework/pch.h
+++ VulkanFramework/pch.h
@@ -4,8 +4,8 @@
 #define VULKAN_FRAMEWORK_PCH_H
 
 #include <VulkanFramework/NonCopyable.h>
+#include <VulkanFramework/ForwardDeclarations.h>
 #include <vector>
-#include <memory>
 
 #pragma warning (push)
 #pragma warning (disable :  4191)
@@ -27,6 +27,9 @@
 #pragma warning (disable : 26812)
 
 #define GLM_FORCE_SIZE_T_LENGTH
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE 
+#define GLM_FORCE_LEFT_HANDED
+
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
@@ -42,59 +45,5 @@
 #include <tinygltf/tiny_gltf.h>
 
 #pragma warning (pop)
-
-// forward declarations
-namespace vfs
-{
-	class Buffer;
-	class BufferView;
-	class CommandPool;
-	class DescriptorPool;
-	class DescriptorSet;
-	class DescriptorSetLayout;
-	class Device;
-	class Fence;
-	class Framebuffer;
-	class QueryPool;
-	class PipelineBase;
-	class PipelineLayout;
-	class GraphicsPipeline;
-	class ComputePipeline;
-	class Queue;
-	class Renderer;
-	class RenderPass;
-	class Sampler;
-	class Scene;
-	class SwapChain;
-	class Image;
-	class ImageView;
-	class Semaphore;
-	class Window;
-
-	using BufferPtr				 = std::shared_ptr<Buffer>;
-	using BufferViewPtr			 = std::shared_ptr<BufferView>;
-	using CommandPoolPtr		 = std::shared_ptr<CommandPool>;
-	using DescriptorPoolPtr		 = std::shared_ptr<DescriptorPool>;
-	using DescriptorSetPtr		 = std::shared_ptr<DescriptorSet>;
-	using DescriptorSetLayoutPtr = std::shared_ptr<DescriptorSetLayout>;
-	using DevicePtr				 = std::shared_ptr<Device>;
-	using FencePtr				 = std::shared_ptr<Fence>;
-	using FramebufferPtr		 = std::shared_ptr<Framebuffer>;
-	using QueryPoolPtr			 = std::shared_ptr<QueryPool>;
-	using PipelineBasePtr		 = std::shared_ptr<PipelineBase>;
-	using PipelineLayoutPtr		 = std::shared_ptr<PipelineLayout>;
-	using GraphicsPipelinePtr	 = std::shared_ptr<GraphicsPipeline>;
-	using ComputePipelinePtr	 = std::shared_ptr<ComputePipeline>;
-	using QueuePtr				 = std::shared_ptr<Queue>;
-	using RendererPtr			 = std::shared_ptr<Renderer>;
-	using RenderPassPtr			 = std::shared_ptr<RenderPass>;
-	using SamplerPtr			 = std::shared_ptr<Sampler>;
-	using ScenePtr				 = std::shared_ptr<Scene>;
-	using SwapChainPtr			 = std::shared_ptr<SwapChain>;
-	using ImagePtr				 = std::shared_ptr<Image>;
-	using ImageViewPtr			 = std::shared_ptr<ImageView>;
-	using WindowPtr				 = std::shared_ptr<Window>;
-	using SemaphorePtr			 = std::shared_ptr<Semaphore>;
-};
 
 #endif
